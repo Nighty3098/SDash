2f684220b889622cc955efdf27f2c36b8a168bd0
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-11-05 23:08:50 +0700

    Fix

--- app/config.py
+++ app/config.py
@@ -4,18 +4,13 @@ from logging.config import dictConfig
 
 from flask import Flask
 
-home_dir = os.path.expanduser("~")
-log_file_path = os.path.join(home_dir, "logs/SDashPanel.log")
-
 app = Flask(__name__)
 
 app_version = "0.3.6"
-
 
 logging.basicConfig(
     encoding="utf-8",
     datefmt="%m/%d/%Y %I:%M:%S %p",
     level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(name)s | %(threadName)s : %(message)s",
-    handlers=[logging.FileHandler(log_file_path), logging.StreamHandler()],
+    format="%(asctime)s | %(levelname)s | %(name)s | %(threadName)s : %(message)s"
 )
